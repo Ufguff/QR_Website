ad4d61c985fe1df36b07354b4493e7e95f5a859d
Vildan Khismatullin <khismatullin.vildan@mail.ru>
2024-08-19 20:20:00 +0500

    fix: adaptive layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   const [textToQR, setTextToQR] = useState<string>("")
 
   return (
-    <div id="app" className="full-size content-center">
+    <div id="app" className="full-size">
       <div className="qr-form">
         <div className="qr-form-wrapper">
           <div className="qr-form-edit">
@@ -18,8 +18,10 @@ function App() {
             <button id="create-button" className="btn">Создать QR-Код</button>
           </div>
           <div className="qr-form-answer">
-            <div className="qr-form-answer-image">
-              <img src="" alt="" width="250" height="250"/>
+            <div className="qr-form-answer-image content-center">
+              <div className="qr-form-answer-image-bg">
+                <img src="" alt=""/>
+              </div>
             </div>
             <div className="qr-form-answer-buttons">
               <button className="btn download-button">Скачать PNG</button>
